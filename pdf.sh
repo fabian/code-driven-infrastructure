f034af3aa530b4eedf0307463dcdd7c9212e9e14
Fabian Vogler <fabian@equivalence.ch>
2015-03-27 11:31:11 +0100

    Add build number

--- pdf.sh
+++ pdf.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-prince --javascript index.html -o code-driven-infrastructure-and-deployment.pdf && open code-driven-infrastructure-and-deployment.pdf
+
+prince --javascript --script=js/build.js index.html -o code-driven-infrastructure-and-deployment.pdf && open code-driven-infrastructure-and-deployment.pdf
+
+if [ -f js/build.js ]; then
+    build=$(awk -F'".?' '{print $1 "\042." $2+1 "\042;\n"}' js/build.js)
+    echo $build > js/build.js
+fi
